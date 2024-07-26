a95f51fa0a28ea2d8961241fd3667dabc4232d70
Sangharsha Chaulagain <sangharsha.chaulagain@gmail.com>
2024-07-26 12:25:31 +1000

    add navbar

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  toggle() {
+    const el = document.getElementById("mobile-links")
+    const barEl = document.getElementById("bars")
+    const closeEl = document.getElementById("close")
+
+    el.classList.toggle("hidden")
+    barEl.classList.toggle("hidden")
+    closeEl.classList.toggle("hidden")
+  }
+}
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :admins, skip: [:registrations]
+  devise_for :users
+  resources :courses do 
+    resources :lessons
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  authenticated :admin_user do 
+    root to: "admin#index", as: :admin_root
+  end
+
+  get "admin" => "admin#index"
+  
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to: "courses#index"
+end
